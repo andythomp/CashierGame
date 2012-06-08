73397fd8e0428849c0961072cb14da90d60f3333
Andrew Thompson <andrew.thompson@live.ca>
2012-06-08 19:10:22 -0400

    Added score and json levels

--- Rect.js
+++ Rect.js
@@ -5,6 +5,7 @@
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		this.item = null;
 	}
 
 	Rect.prototype = new Container();
@@ -13,7 +14,7 @@
 	Rect.prototype.y;
 	Rect.prototype.width;
 	Rect.prototype.height;
-	Rect.prototype.height;
+	Rect.prototype.item;
 
 	Rect.prototype.Container_initialize = Rect.prototype.initialize;
 		Rect.prototype.initialize = function() {
