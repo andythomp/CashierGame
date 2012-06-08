c57e0845f8f12c6eaca27549e984c11d768793a3
Andrew Thompson <andrew.thompson@live.ca>
2012-06-08 14:41:39 -0400

    Cloned from non git repo

---
+++ Rect.js
@@ -0,0 +1,24 @@
+(function(window) {
+
+	function Rect(x, y, width, height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	Rect.prototype = new Container();
+
+	Rect.prototype.x;
+	Rect.prototype.y;
+	Rect.prototype.width;
+	Rect.prototype.height;
+	Rect.prototype.height;
+
+	Rect.prototype.Container_initialize = Rect.prototype.initialize;
+		Rect.prototype.initialize = function() {
+			this.Container_initialize();
+		}
+
+	window.Rect = Rect;
+}(window));
